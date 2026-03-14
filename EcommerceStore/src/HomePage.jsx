4b823657c5c3b7554daef584291ab8c19ec60588
Hemanth N <hemanthn270@gmail.com>
2026-03-14 23:50:12 +0530

    made a list of an product objects which has uniqueid,name,rating stars ,rating counts,image,image for each and using this we generated html and listed products on homepage

--- EcommerceStore/src/HomePage.jsx
+++ EcommerceStore/src/HomePage.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { useEffect } from "react";
 import "./HomePage.css";
 import Header from "./components/header";
-
+import { products } from './components/products'
 function HomePage() {
 
   useEffect(() => {
@@ -16,29 +16,29 @@ function HomePage() {
       <div className="home-page">
         <div className="products-grid">
 
-          {/* Product 1 */}
-
-          <div className="product-container">
+{products.map((product)=>{
+return(
+      <div key={product.id} className="product-container">
             <div className="product-image-container">
               <img
                 className="product-image"
-                src="/images/products/athletic-cotton-socks-6-pairs.jpg"
+                src={product.image}
               />
             </div>
 
             <div className="product-name limit-text-to-2-lines">
-              Black and Gray Athletic Cotton Socks - 6 Pairs
+              {product.name}
             </div>
 
             <div className="product-rating-container">
               <img
                 className="product-rating-stars"
-                src="/images/ratings/rating-45.png"
+                src={`/images/ratings/rating-${product.rating.stars*10}.png`}
               />
-              <div className="product-rating-count link-primary">87</div>
+              <div className="product-rating-count link-primary">{product.rating.count}</div>
             </div>
 
-            <div className="product-price">$10.90</div>
+            <div className="product-price">{(product.priceCents/100).toFixed(2)}</div>
 
             <div className="product-quantity-container">
               <select>
@@ -59,97 +59,8 @@ function HomePage() {
               Add to Cart
             </button>
           </div>
-
-
-          {/* Product 2 */}
-
-          <div className="product-container">
-            <div className="product-image-container">
-              <img
-                className="product-image"
-                src="/images/products/intermediate-composite-basketball.jpg"
-              />
-            </div>
-
-            <div className="product-name limit-text-to-2-lines">
-              Intermediate Size Basketball
-            </div>
-
-            <div className="product-rating-container">
-              <img
-                className="product-rating-stars"
-                src="/images/ratings/rating-40.png"
-              />
-              <div className="product-rating-count link-primary">127</div>
-            </div>
-
-            <div className="product-price">$20.95</div>
-
-            <div className="product-quantity-container">
-              <select>
-                {[1,2,3,4,5,6,7,8,9,10].map((num)=>(
-                  <option key={num}>{num}</option>
-                ))}
-              </select>
-            </div>
-
-            <div className="product-spacer"></div>
-
-            <div className="added-to-cart">
-              <img src="/images/icons/checkmark.png" />
-              Added
-            </div>
-
-            <button className="add-to-cart-button button-primary">
-              Add to Cart
-            </button>
-          </div>
-
-
-          {/* Product 3 */}
-
-          <div className="product-container">
-            <div className="product-image-container">
-              <img
-                className="product-image"
-                src="/images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg"
-              />
-            </div>
-
-            <div className="product-name limit-text-to-2-lines">
-              Adults Plain Cotton T-Shirt - 2 Pack
-            </div>
-
-            <div className="product-rating-container">
-              <img
-                className="product-rating-stars"
-                src="/images/ratings/rating-45.png"
-              />
-              <div className="product-rating-count link-primary">56</div>
-            </div>
-
-            <div className="product-price">$7.99</div>
-
-            <div className="product-quantity-container">
-              <select>
-                {[1,2,3,4,5,6,7,8,9,10].map((num)=>(
-                  <option key={num}>{num}</option>
-                ))}
-              </select>
-            </div>
-
-            <div className="product-spacer"></div>
-
-            <div className="added-to-cart">
-              <img src="/images/icons/checkmark.png" />
-              Added
-            </div>
-
-            <button className="add-to-cart-button button-primary">
-              Add to Cart
-            </button>
-          </div>
-
+)
+})}
         </div>
       </div>
     </>
