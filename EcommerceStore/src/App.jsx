75b1835cb8bcf927096ce5b441ca6e0d03f782bc
Hemanth N <hemanthn270@gmail.com>
2026-03-14 21:04:17 +0530

    Made use of routes and made a checkout page

--- EcommerceStore/src/App.jsx
+++ EcommerceStore/src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 import HomePage from './HomePage'
+import{ Routes , Route } from 'react-router-dom'
 import './App.css'
-
+import  CheckoutPage  from './CheckoutPage'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <HomePage />
+    <Routes>
+       <Route index path='/' element={<HomePage />} />
+       <Route path='/CheckoutPage' element={<CheckoutPage />} />
+
+     
+     </Routes>
     </>
   )
 }
--- EcommerceStore/src/HomePage.jsx
+++ EcommerceStore/src/HomePage.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 import './HomePage.css'
 import './header.css'
 const HomePage = () => {
   return (
-    <div>
+    <>
+     <title>Ecommerce-Shop</title>
          <div className="header">
       <div className="left-section">
-        <a href="index.html" className="header-link">
-          <img className="logo"
+<Link to="/" className="header-link">   
+       <img className="logo"
             src="images/logo-white.png" />
           <img className="mobile-logo"
             src="images/mobile-logo-white.png" />
-        </a>
+        </Link>
       </div>
 
       <div className="middle-section">
@@ -23,16 +25,14 @@ const HomePage = () => {
       </div>
 
       <div className="right-section">
-        <a className="orders-link header-link" href="orders.html">
-
+<Link className="orders-link header-link" to="/orders">
           <span className="orders-text">Orders</span>
-        </a>
+        </Link>
 
-        <a className="cart-link header-link" href="checkout.html">
-          <img className="cart-icon" src="images/icons/cart-icon.png" />
+<Link className="cart-link header-link" to="/CheckoutPage">          <img className="cart-icon" src="images/icons/cart-icon.png" />
           <div className="cart-quantity">3</div>
           <div className="cart-text">Cart</div>
-        </a>
+        </Link>
       </div>
     </div>
 
@@ -186,7 +186,7 @@ const HomePage = () => {
         </div>
       </div>
     </div>
-    </div>
+    </>
   )
 }
 
